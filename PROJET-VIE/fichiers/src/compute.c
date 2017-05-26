ace6cf6a3b2a7b69356ee81785a321e88c5c7d81
Virgile Robles <virgile.robles@protonmail.ch>
2017-05-26 17:45:40 +0200

    Pas mal d'optimisations mais ça accélère pas beaucoup

--- PROJET-VIE/fichiers/src/compute.c
+++ PROJET-VIE/fichiers/src/compute.c
@@ -107,18 +107,36 @@ unsigned compute_v0 (unsigned nb_iter)
 
 #define TILE_X (32)
 #define TILE_Y (32)
+#define TILENB_X (DIM / TILE_X)
+#define TILENB_Y (DIM / TILE_Y)
 
-void computeOneTile(int xT, int yT) {
+void minMax(int x, int y, int* xMin, int* xMax, int* yMin, int* yMax) {
+    if(*xMin == -1) { //First
+	*xMin = TILE_X;
+	*yMin = TILE_Y;
+    }
+    if(x < *xMin) *xMin = x;
+    if(y < *yMin) *yMin = y;
+    if(x > *xMax) *xMax = x;
+    if(y > *yMax) *yMax = y;
+}
+
+void computeOneTile(int xT, int yT, int* xMin, int* xMax, int* yMin, int* yMax) {
+    if(xMin != NULL) *xMin = *xMax = *yMin = *yMax = -1;
     for (int x = 0; x < TILE_X; x++)
 	for (int y = 0; y < TILE_Y; y++){
 	    int i = xT * TILE_X + x;
 	    int j = yT * TILE_Y + y;
 	    int count = neighborCount(i, j);
 
-	    if(cur_img(i, j) != 0 && (count != 2 && count != 3))
+	    if(cur_img(i, j) != 0 && (count != 2 && count != 3)) {
 		next_img(i, j) = 0;
-	    else if(cur_img(i, j) == 0 && count == 3)
+		if(xMin != NULL) minMax(x, y, xMin, xMax, yMin, yMax);
+	    }
+	    else if(cur_img(i, j) == 0 && count == 3) {
 		next_img(i, j) = couleur;
+		if(xMin != NULL) minMax(x, y, xMin, xMax, yMin, yMax);
+	    }
 	    else next_img (i, j) = cur_img (i, j);
 	}
 }
@@ -129,33 +147,21 @@ void computeOneTile(int xT, int yT) {
 unsigned compute_v1(unsigned nb_iter){
     for (unsigned it = 1; it <= nb_iter; it ++) {
 
-	for (int xT = 0; xT < DIM/TILE_X; xT++)
-	    for (int yT = 0; yT < DIM/TILE_Y; yT++)
-		computeOneTile(xT, yT);
+	for (int xT = 0; xT < TILENB_X; xT++)
+	    for (int yT = 0; yT < TILENB_Y; yT++)
+		computeOneTile(xT, yT, NULL, NULL, NULL, NULL);
 	swap_images ();
     }
 
     return 0; // on ne s'arrête jamais
 }
 
-void setMayChange(int matrix[DIM/TILE_X][DIM/TILE_Y], int xT, int yT, int x, int y){
-    // xT
-    matrix[xT][yT] = 1;
-    if(y == 0 && yT > 0) matrix[xT][yT-1] = 1;
-    else if(y == TILE_Y-1 && yT < DIM/TILE_Y - 1) matrix[xT][yT+1] = 1;
-
-    // xT-1
-    if(x == 0 && xT != 0){
-	matrix[xT-1][yT] = 1;
-	if(y == 0 && yT > 0) matrix[xT-1][yT-1] = 1;
-	else if(y == TILE_Y-1 && yT < DIM/TILE_Y - 1) matrix[xT-1][yT+1] = 1;
-    }
-    // xT+1
-    else if(x == TILE_X-1 && xT != DIM/TILE_X - 1){
-	matrix[xT+1][yT] = 1;
-	if(y == 0 && yT > 0) matrix[xT+1][yT-1] = 1;
-	else if(y == TILE_Y-1 && yT < DIM/TILE_Y - 1) matrix[xT+1][yT+1] = 1;
-    }
+void setMayChange(int* matrix, int xMin, int xMax,
+	int yMin, int yMax){
+    if(xMin == -1) return;
+    for(int i = xMin / TILE_X ; i < xMax / TILE_X ; ++i) 
+	for(int j = yMin / TILE_Y ; j < yMax / TILE_Y ; ++j) 
+	    matrix[i * TILENB_X + j] = 0;
 }
 
 
@@ -163,26 +169,27 @@ void setMayChange(int matrix[DIM/TILE_X][DIM/TILE_Y], int xT, int yT, int x, int
  * Version séquentielle optimisée
  */
 unsigned compute_v2(unsigned nb_iter) {
-    int mayChangeCurr[DIM/TILE_X][DIM/TILE_Y];
-    int mayChangeNext[DIM/TILE_X][DIM/TILE_Y];
+    int* mayChange1 = malloc(TILENB_Y * TILENB_X * sizeof(int));
+    int* mayChange2 = malloc(TILENB_Y * TILENB_X * sizeof(int));
+    int *cur = mayChange1, *next = mayChange2;
+    int xMin, xMax, yMin, yMax; 
 
-    // Initialize mayChangeCurr at 0.
-    for (int xT = 0; xT < DIM/TILE_X; xT++)
-	for (int yT = 0; yT < DIM/TILE_Y; yT++)
-	    mayChangeCurr[xT][yT] = 1;
+    // Initially, everything is computed (0)
+    for (int i = 0; i < TILENB_X * TILENB_Y; i++) cur[i] = 0;
 
     for (unsigned it = 1; it <= nb_iter; it ++) {
-	// Reset mayChangeNext to 0.
-	for (int xT = 0; xT < DIM/TILE_X; xT++)
-	    for (int yT = 0; yT < DIM/TILE_Y; yT++)
-		mayChangeNext[xT][yT] = 0;
+	// Ideally, the next frame is not computed at all (1)
+	for (int i = 0; i < TILENB_X * TILENB_Y; i++) next[i] = 1;
 
 	// Compute
-	for (int xT = 0; xT < DIM/TILE_X; xT++)
-	    for (int yT = 0; yT < DIM/TILE_Y; yT++){
+	for (int xT = 0; xT < TILENB_X; xT++)
+	    for (int yT = 0; yT < TILENB_Y; yT++){
 		// No computing if not needed
-		if(!mayChangeCurr[xT][yT]){ 
-		    //Copy the tile
+		if(cur[xT * TILENB_X + yT] != 0){ 
+		    next[xT * TILENB_X + yT] = cur[xT * TILENB_X + yT] + 1;
+		    if(cur[xT * TILENB_X + yT] > 2) continue;
+		    
+		    //Copy the tile only for the first two swaps
 		    for (int x = 0; x < TILE_X; x++)
 			for (int y = 0; y < TILE_Y; y++){
 			    int i = xT * TILE_X + x;
@@ -193,32 +200,20 @@ unsigned compute_v2(unsigned nb_iter) {
 		}
 
 		//Iterate over the tile
-		for (int x = 0; x < TILE_X; x++)
-		    for (int y = 0; y < TILE_Y; y++){
-			int i = xT * TILE_X + x;
-			int j = yT * TILE_Y + y;
-			int count = neighborCount(i, j);
-
-			if(cur_img(i, j) != 0 && (count != 2 && count != 3)){
-			    next_img(i, j) = 0;
-			    //A change occured
-			    setMayChange(mayChangeNext, xT, yT, x, y);
-			} else if(cur_img(i, j) == 0 && count == 3){
-			    next_img(i, j) = couleur;
-			    //A change occured
-			    setMayChange(mayChangeNext, xT, yT, x, y);
-			} else
-			    next_img (i, j) = cur_img (i, j);
-		    }
+		computeOneTile(xT, yT, &xMin, &xMax, &yMin, &yMax);
+		//Report changes made
+		setMayChange(next, xMin, xMax, yMin, yMax);
 	    }
 
-	// Copy mayChangeNext to mayChangeCurr
-	for (int xT = 0; xT < DIM/TILE_X; xT++)
-	    for (int yT = 0; yT < DIM/TILE_Y; yT++)
-		mayChangeCurr[xT][yT] = mayChangeNext[xT][yT];
+	//Swap buffers
+	int* tmp = cur;
+	cur = next;
+	next = tmp;
 
 	swap_images ();
     }
+    free(mayChange1);
+    free(mayChange2);
     return 0;
 }
 
