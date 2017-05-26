8ed74339a8b23866ea6a7898072298a4ed16ae19
LE DILY Hugo <hledily@villars.pedago.ipb.fr>
2017-05-26 12:47:06 +0200

    version seq tuilée + seq opti

--- PROJET-VIE/fichiers/src/compute.c
+++ PROJET-VIE/fichiers/src/compute.c
@@ -109,29 +109,116 @@ unsigned compute_v0 (unsigned nb_iter)
 /**
  * Version séquentielle tuilée
  */
-unsigned compute_v1(unsigned nb_iter)
-{
+unsigned compute_v1(unsigned nb_iter){
+	int tileX = 32;
+	int tileY = 32;
+	
     for (unsigned it = 1; it <= nb_iter; it ++) {
-	for (int i = 0; i < DIM; i++)
-	    for (int j = 0; j < DIM; j++){
+	
+	for (int xT = 0; xT < DIM/tileX; xT++)
+	for (int yT = 0; yT < DIM/tileY; yT++)
+	for (int x = 0; x < tileX; x++)
+	for (int y = 0; y < tileY; y++){
+		int i = xT * tileX + x;
+		int j = yT * tileY + y;
 		int count = neighborCount(i, j);
+					
 		if(cur_img(i, j) != 0 && (count != 2 && count != 3))
-		    next_img(i, j) = 0;
+			next_img(i, j) = 0;
 		else if(cur_img(i, j) == 0 && count == 3)
-		    next_img(i, j) = couleur;
+			next_img(i, j) = couleur;
 		else next_img (i, j) = cur_img (i, j);
-	    }
+	}
 
 	swap_images ();
     }
-    return 0;
+    
     return 0; // on ne s'arrête jamais
 }
+
+
+/// 1. Ca serait pas mal de savoir ou tileX, tileY sont définie (pour l'instant pas globale je crois)
+/// 2. A partir de la, on peut avoir mayChangeNext (matrix) en globale aussi.
+void setMayChange(int* matrix, int tileX, int tileY, int xT, int yT, int x, int y){
+	// xT
+	matrix[xT][yT] = 1;
+	if(y == 0 && yT > 0) matrix[xT][yT-1] = 1;
+	else if(y == tileY-1 && yT < DIM/tileY - 1) matrix[xT][yT+1] = 1;
+	
+	// xT-1
+	if(x == 0 && xT != 0){
+		matrix[xT-1][yT] = 1;
+		if(y == 0 && yT > 0) matrix[xT-1][yT-1] = 1;
+		else if(y == tileY-1 && yT < DIM/tileY - 1) matrix[xT-1][yT+1] = 1;
+	}
+	// xT+1
+	else if(x == tileX-1 && xT == DIM/tileX - 1){
+		matrix[xT+1][yT] = 1;
+		if(y == 0 && yT > 0) matrix[xT+1][yT-1] = 1;
+		else if(y == tileY-1 && yT < DIM/tileY - 1) matrix[xT+1][yT+1] = 1;
+	}
+}
+
 
 /**
  * Version séquentielle optimisée
  */
 unsigned compute_v2(unsigned nb_iter) {
+	int tileX = 32;
+	int tileY = 32;
+	int mayChangeCurr[DIM/tileX][DIM/tileY];
+	int mayChangeNext[DIM/tileX][DIM/tileY];
+	
+	// Initialize mayChangeCurr at 0.
+	for (int xT = 0; xT < DIM/tileX; xT++)
+	for (int yT = 0; yT < DIM/tileY; yT++)
+	mayChangeCurr[xT][yT] = 0;
+	
+    for (unsigned it = 1; it <= nb_iter; it ++) {
+	// Reset mayChangeNext to 0.
+	for (int xT = 0; xT < DIM/tileX; xT++)
+	for (int yT = 0; yT < DIM/tileY; yT++)
+	mayChangeNext[xT][yT] = 0;
+	
+	// Compute
+	for (int xT = 0; xT < DIM/tileX; xT++)
+	for (int yT = 0; yT < DIM/tileY; yT++){
+		if(!mayChangeCurr[xT,yT]){ // No computing if not needed
+			for (int x = 0; x < tileX; x++)
+			for (int y = 0; y < tileY; y++){
+				int i = xT * tileX + x;
+				int j = yT * tileY + y;
+				next_img (i, j) = cur_img (i, j);
+			}
+			
+			continue; 
+		}
+	
+		for (int x = 0; x < tileX; x++)
+		for (int y = 0; y < tileY; y++){
+			int i = xT * tileX + x;
+			int j = yT * tileY + y;
+			int count = neighborCount(i, j);
+			
+			if(cur_img(i, j) != 0 && (count != 2 && count != 3)){
+				next_img(i, j) = 0;
+				setMayChange(mayChangeNext,tileX,tileY,xT,yT,x,y);
+			} else if(cur_img(i, j) == 0 && count == 3){
+				next_img(i, j) = couleur;
+				setMayChange(mayChangeNext,tileX,tileY,xT,yT,x,y);
+			} else
+				next_img (i, j) = cur_img (i, j);
+		}
+	}
+	
+	// Copy mayChangeNext to mayChangeCurr
+	for (int xT = 0; xT < DIM/tileX; xT++)
+	for (int yT = 0; yT < DIM/tileY; yT++)
+	mayChangeCurr[xT][yT] = mayChangeNext[xT][yT];
+
+	swap_images ();
+    }
+	
     return 0;
 }
 
